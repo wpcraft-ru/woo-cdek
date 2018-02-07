5e3e593dbb2c5d02662fd3b58cd4916ee822fb87
yumashev <yumashev@fleep.io>
2018-02-07 16:47:00 +0300

    init commit

---
+++ woocdek.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WooCommerce CDEK
+Plugin URI: https://github.com/uptimizt/woocommerce-cdek
+Description: WooCommerce & СДЭК - интеграция.
+Author: WPCraft
+Author URI: http://wpcraft.ru/?utm_source=wpplugin&utm_medium=plugin-link&utm_campaign=WooAmoConnector
+Version: 0.1
+*/
